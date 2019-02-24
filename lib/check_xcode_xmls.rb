4b97b402799df44bad89eff9d69c916eb8b27f38
Andrei Nagy <nagy.andrei@gmail.com>
2019-02-24 10:51:53 +0200

    Initial commit.

---
+++ lib/check_xcode_xmls.rb
@@ -0,0 +1,6 @@
+require "check_xcode_xmls/version"
+
+module CheckXcodeXmls
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/check_xcode_xmls/version.rb
@@ -0,0 +1,3 @@
+module CheckXcodeXmls
+  VERSION = "0.1.0"
+end
